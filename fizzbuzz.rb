6de05b39f1fb2bbc2b81ca2abbb7a78aefb608e6
Nick Lanese <nlanese21@gmail.com>
2020-02-17 16:27:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(n)
+  if (n % 3 == 0) && (n % 5 == 0)
+    return "fizzbuzz"
+  elsif n % 3
+    return "fizz"
+  elsif n % 5
+    return "buzz"
+  else
+    return nil
+  end
