38e105d635e8e8f14c7a4756a91b36ba37eef0b2
Nick Lanese <nlanese21@gmail.com>
2020-02-17 16:31:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(n)
   if (n % 3 == 0) && (n % 5 == 0)
     return "FizzBuzz"
-  elsif n % 3
+  elsif n % 3 == 0
     return "Fizz"
-  elsif n % 5
+  elsif n % 5 == 0
     return "Buzz"
   else
     return nil
